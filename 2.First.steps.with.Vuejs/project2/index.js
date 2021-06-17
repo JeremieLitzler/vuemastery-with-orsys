0b99470cd1124bd032cbd2fe14028a24552d315b
Jeremie Litzler <jeremie.litzler@conduent.com>
2021-06-17 10:33:19 +0200

    continue the app

--- 2.First.steps.with.Vuejs/project2/index.js
+++ 2.First.steps.with.Vuejs/project2/index.js
@@ -1,31 +1,58 @@
 const App = {
   data() {
     return {
+      validationError: false,
+      newMovieFormDisplayed: false,
       movies: [
         {
           id: 1,
           name: 'Movie 1',
-          description: 'Description of movie 1',
+          description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat',
           modifying: false,
         },
         {
           id: 2,
           name: 'Movie 2',
-          description: 'Description of movie 2',
+          description:
+            'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur',
           modifying: false,
         },
         {
           id: 3,
           name: 'Movie 3',
-          description: 'Description of movie 3',
+          description:
+            'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat',
           modifying: false,
         },
       ],
     };
   },
+  computed: {
+    maxId() {
+      return this.movies ? this.movies.length++ : 1;
+    },
+    movieCount() {
+      return this.movies ? this.movies.length : 0;
+    },
+  },
   methods: {
-    addMovie() {
-      this.movies.push();
+    addMovie(name, description) {
+      console.log('Name', name, 'Description', description);
+      if (name === undefined || description === undefined) {
+        this.validationError = true;
+        return;
+      }
+      const newMovieId = this.movies ? this.movies.length++ : 0;
+      console.log('New Movie Id', newMovieId);
+      const newMovie = {
+        id: newMovieId,
+        name,
+        description,
+        modifying: false,
+      };
+      console.log('Movie', newMovie);
+      this.movies.push(newMovie);
     },
     removeMovie(movieToRemove) {
       if (!movieToRemove) throw new Error('parameter movie is mandatory!');
@@ -48,6 +75,9 @@ const App = {
         matchingMovie = movieModified;
         console.log('Modify the movie ...', matchingMovie);
       }
+    },
+    showNewMovieForm() {
+      this.newMovieFormDisplayed = true;
     },
   },
 };
